9b750cbab66030ac9635602bbf08864ab2bab14d
Sword <330132662@qq.com>
2025-03-14 11:25:51 +0800

    目录纠正

---
+++ miniprogram/siteinfo.js
@@ -0,0 +1,7 @@
+module.exports = {
+    version: "1.3.2",
+    wid: "1",// saas用户id
+    module: "wifi",
+    host: "https://host/",
+    baseUrl: "https://host/wifi/user",
+};
--- siteinfo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  version: "1.3.2",
-  wid: "1",
-  module: "wifi",
-  host: "https://host/",
-  baseUrl: "https://host/wifi/user",
-};
