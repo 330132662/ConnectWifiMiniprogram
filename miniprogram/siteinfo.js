5c0eeb2beee76adc297d472c8e4cae96b1d85389
Sword <330132662@qq.com>
2025-03-15 17:43:20 +0800

    权限补充

--- miniprogram/siteinfo.js
+++ miniprogram/siteinfo.js
@@ -2,6 +2,6 @@ module.exports = {
     version: "1.3.2",
     wid: "1",// saas用户id
     module: "wifi",
-    host: "https://host/",
-    baseUrl: "https://host/wifi/user",
+    host: "https://wifi.x.cn/",
+    baseUrl: "https://wifi.x.cn/wifi/user",
 };
